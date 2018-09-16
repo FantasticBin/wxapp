5092f379ec938e250f2a48de892ebfbc39cdf5d9
fangbin.li <lfb370921@163.com>
2018-09-16 16:48:24 +0800

    -[增加] 自动更新

--- pages/calculate/calculate.js
+++ pages/calculate/calculate.js
@@ -45,6 +45,7 @@ Page({
    */
   onShow: function(options) {
     this.loadData();
+    this.checkUpdate();
   },
   loadData() {
     let data = wx.getStorageSync(staticData.SAVED_FUND_LIST)
@@ -340,5 +341,24 @@ Page({
       }
       return real;
     }
+  },
+  checkUpdate() {
+    const updateManager = wx.getUpdateManager()
+    updateManager.onCheckForUpdate(function(res) {
+      // 请求完新版本信息的回调
+      console.log(res.hasUpdate)
+    })
+    updateManager.onUpdateReady(function() {
+      wx.showModal({
+        title: '更新提示',
+        content: '新版本已经准备好，是否重启应用？',
+        success: function(res) {
+          if (res.confirm) {
+            // 新的版本已经下载好，调用 applyUpdate 应用新版本并重启
+            updateManager.applyUpdate()
+          }
+        }
+      })
+    })
   }
 })
